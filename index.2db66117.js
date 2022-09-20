bc4b1d71fa018abea56fed9cdd8e01b8c922cdb2
vokur13 <103522917+vokur13@users.noreply.github.com>
2022-09-20 06:03:58 +0000

    Deploying to gh-pages from @ vokur13/goit-js-hw-folder@4c9a1226eb89dccaf62f629d0363c7c44a5fdbb2 🚀

---
+++ index.2db66117.js
@@ -0,0 +1,2 @@
+console.log("js-hw-folder"),console.log("Well done");
+//# sourceMappingURL=index.2db66117.js.map
---
+++ index.66186542.js
@@ -0,0 +1,2 @@
+console.log("js-hw-folder"),console.log("Well done");
+//# sourceMappingURL=index.66186542.js.map
